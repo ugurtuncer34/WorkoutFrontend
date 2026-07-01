9c5e62110ac70f9ded8ecbadce53075f633e6d96
theugurtuncer <ugurtuncer93@icloud.com>
2026-07-01 16:15:54 +0300

    Env api url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // .NET backend (check Properties/launchSettings.json)
-const BASE_URL = 'http://localhost:5065/api'; 
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5065/api'; 
 
 export const api = axios.create({
     baseURL: BASE_URL,
